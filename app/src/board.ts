180d7a684310e2bac50f62050ad8e243d0e233f6
adrian.spirescu <adrian.spirescu@nagarro.com>
2023-03-24 12:56:24 +0100

    zoom and pan stage

--- app/src/board.ts
+++ app/src/board.ts
@@ -159,10 +159,44 @@ export function initBoard() {
     stage = new Konva.Stage({
         container: 'container',
         width: window.innerWidth,
-        height: window.innerHeight - 60
+        height: window.innerHeight - 60,
+        draggable: true
     })
     layer = new Konva.Layer()
     stage.add(layer)
     layer.draw()
+
+    const scaleBy = 1.05
+    stage.on('wheel', (e) => {
+        // stop default scrolling
+        e.evt.preventDefault();
+
+        var oldScale = stage.scaleX();
+        var pointer = stage.getPointerPosition();
+
+        var mousePointTo = {
+            x: (pointer.x - stage.x()) / oldScale,
+            y: (pointer.y - stage.y()) / oldScale,
+        };
+
+        // how to scale? Zoom in? Or zoom out?
+        let direction = e.evt.deltaY > 0 ? 1 : -1;
+
+        // when we zoom on trackpad, e.evt.ctrlKey is true
+        // in that case lets revert direction
+        if (e.evt.ctrlKey) {
+            direction = -direction;
+        }
+
+        var newScale = direction > 0 ? oldScale * scaleBy : oldScale / scaleBy;
+
+        stage.scale({ x: newScale, y: newScale });
+
+        var newPos = {
+            x: pointer.x - mousePointTo.x * newScale,
+            y: pointer.y - mousePointTo.y * newScale,
+        };
+        stage.position(newPos);
+    });
 
 }
